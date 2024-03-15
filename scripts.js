11810d09fab74de1d88f3526c34a6eb784142c06
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2024-03-15 11:55:35 -0400

    created index.html, css file, server and scripts file, started initial code to attach userMedia functionality to the project

---
+++ scripts.js
@@ -0,0 +1,19 @@
+
+
+const localVideoEl = document.querySelector('#local-video');
+const remoteVideoEl = document.querySelector('#remote-video');
+
+let localStream; // var to hold the local video stream
+let remoteStream; // var to hold the remote video stream
+let peerConnection; // the peerConnection that the two clients use to talk
+
+// when a client initiates a call
+const call = async (e) => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        // audio: true
+    });
+    localVideoEl.srcObject = stream;
+}
+
+document.querySelector('#call').addEventListener('click', call);

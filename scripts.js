a76a460cf4b5552bfb7c2f4bbf9e8c4e323f2e3c
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2024-03-15 14:19:14 -0400

    added STUN server URLs, created the call function, created an offer and logged the data, added icecandidate event listener to create a peer connection

--- scripts.js
+++ scripts.js
@@ -7,6 +7,17 @@ let localStream; // var to hold the local video stream
 let remoteStream; // var to hold the remote video stream
 let peerConnection; // the peerConnection that the two clients use to talk
 
+let peerConfiguration = {
+    iceServers: [
+        {
+            urls: [
+                'stun:stun.l.google.com:19302',
+                'stun:stun1.l.google.com:19302'
+            ]
+        }
+    ]
+}
+
 // when a client initiates a call
 const call = async (e) => {
     const stream = await navigator.mediaDevices.getUserMedia({
@@ -14,6 +25,39 @@ const call = async (e) => {
         // audio: true
     });
     localVideoEl.srcObject = stream;
+    localStream = stream;
+
+    // peerConnection is all set with our STUN servers sent over
+    await createPeerConnection();
+
+    // create offer time
+    try {
+        console.log('creating offer');
+        const offer = await peerConnection.createOffer();
+        console.log(offer);
+        peerConnection.setLocalDescription(offer);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const createPeerConnection = async () => {
+    return new Promise(async (resolve, reject) => {
+        // RTCPeerConnection is the thing that creates the connection
+        // we can pass a config object and that config object can contain STUN servers
+        // which will fetch us ICE candidates 
+        peerConnection = await new RTCPeerConnection(peerConfiguration);
+
+        localStream.getTracks().forEach((track) => {
+            peerConnection.addTrack(track, localStream);
+        })
+
+        peerConnection.addEventListener('icecandidate', (e) => {
+            console.log('.......Ice candidate found.....');
+            console.log(e);
+    });
+    resolve();
+    })
 }
 
 document.querySelector('#call').addEventListener('click', call);
